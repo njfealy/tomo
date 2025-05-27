53bf2d63e9b790ab649d7564618a1d2f49c61dd4
fealypino <njfealy@gmail.com>
2025-05-27 18:32:33 -0500

    Add mocks for mongo ClientSession and transactions
    createPost and deletePost originally only pushed/pulled the Post to the post collection, but recent changes implement starting mongoDB sessions and transactions during which the the DB also pushes/pulls the post from thecreator(user) document's posts field. Additionally, deletePost also deletes all comments belonging to the post. The respective unit tests have been updated to reflect the  changes in the controllers.

--- tomo-back/src/post/__tests__/post-controller.test.ts
+++ tomo-back/src/post/__tests__/post-controller.test.ts
@@ -1,17 +1,31 @@
-import * as postController from "../post-controller";
-import * as postModel from "../post-model";
 import express from "express";
+import * as postController from "@App/post/post-controller";
 
-import { CreatePostRequest } from "../post.types";
+import * as postModel from "@App/post/post-model";
+import * as userModel from "@App/user/user-model";
+import * as commentModel from "@App/comment/comment-model";
+import * as mongo from "@App/utils/mongo";
+
+import { CreatePostRequest } from "@App/post/post.types";
 import { InsertOneResult, ObjectId, WithId } from "mongodb";
 import { ApiError } from "@App/utils/api-error";
 
-jest.mock("../post-model");
+jest.mock("@App/post/post-model");
+jest.mock("@App/user/user-model");
+jest.mock("@App/comment/comment-model");
+jest.mock("@App/utils/mongo");
 
 describe("createPostController", () => {
   let mockReq: Partial<express.Request>;
   let mockRes: Partial<express.Response>;
   let mockNext: express.NextFunction;
+
+  const mockSession = {
+    startTransaction: jest.fn(),
+    commitTransaction: jest.fn(),
+    abortTransaction: jest.fn(),
+    endSession: jest.fn(),
+  };
 
   beforeEach(() => {
     mockReq = {
@@ -25,10 +39,16 @@ describe("createPostController", () => {
   });
 
   it("should return 201 if InsertOneResult acknowledged", async () => {
+    (mongo.getClient as jest.Mock).mockReturnValue({
+      startSession: () => mockSession,
+    });
     (postModel.insertPost as jest.Mock).mockResolvedValue({
       acknowledged: true,
-      insertedId: null,
+      insertedId: new ObjectId(),
     } as unknown as InsertOneResult<postModel.Post>);
+    (userModel.addPostToUser as jest.Mock).mockResolvedValue({
+      acknowledged: true,
+    });
 
     await postController.createPost(
       mockReq as express.Request<{}, {}, CreatePostRequest>,
@@ -36,14 +56,17 @@ describe("createPostController", () => {
       mockNext
     );
 
+    expect(mockSession.commitTransaction).toHaveBeenCalled();
     expect(mockRes.status).toHaveBeenCalledWith(201);
     expect(mockRes.json).toHaveBeenCalledWith({ message: "Post created" });
   });
 
-  it("should return 500 if InsertOneResult unacknowledged", async () => {
+  it("should call next with error if insertPost fails", async () => {
+    (mongo.getClient as jest.Mock).mockReturnValue({
+      startSession: () => mockSession,
+    });
     (postModel.insertPost as jest.Mock).mockResolvedValue({
       acknowledged: false,
-      insertedId: null,
     } as unknown as InsertOneResult<postModel.Post>);
 
     await postController.createPost(
@@ -52,14 +75,22 @@ describe("createPostController", () => {
       mockNext
     );
 
+    expect(mockSession.abortTransaction).toHaveBeenCalled();
     expect(mockNext).toHaveBeenCalledWith(new ApiError("DB Error", 500));
   });
 
-  it("should return 500 if InsertOneResult is null", async () => {
+  it("should call next with error if addToUser fails", async () => {
+    (mongo.getClient as jest.Mock).mockReturnValue({
+      startSession: () => mockSession,
+    });
     (postModel.insertPost as jest.Mock).mockResolvedValue({
+      acknowledged: true,
+      insertedId: new ObjectId(),
+    });
+
+    (userModel.addPostToUser as jest.Mock).mockResolvedValue({
       acknowledged: false,
-      insertedId: null,
-    } as unknown as InsertOneResult<postModel.Post>);
+    });
 
     await postController.createPost(
       mockReq as express.Request<{}, {}, CreatePostRequest>,
@@ -67,6 +98,7 @@ describe("createPostController", () => {
       mockNext
     );
 
+    expect(mockSession.abortTransaction).toHaveBeenCalled();
     expect(mockNext).toHaveBeenCalledWith(new ApiError("DB Error", 500));
   });
 });
@@ -76,8 +108,17 @@ describe("deletePostController", () => {
   let mockRes: Partial<express.Response>;
   let mockNext: express.NextFunction;
 
+  const mockSession = {
+    startTransaction: jest.fn(),
+    commitTransaction: jest.fn(),
+    abortTransaction: jest.fn(),
+    endSession: jest.fn(),
+  };
+
   beforeEach(() => {
-    mockReq = { body: { postId: new ObjectId().toString() } };
+    mockReq = {
+      params: { postId: new ObjectId().toString() },
+    };
     mockRes = {
       status: jest.fn().mockReturnThis(),
       json: jest.fn(),
@@ -85,83 +126,134 @@ describe("deletePostController", () => {
     mockNext = jest.fn();
   });
 
-  it("should return 204 if deletePostById found Post", async () => {
+  it("should return 204 if success", async () => {
+    (mongo.getClient as jest.Mock).mockReturnValue({
+      startSession: () => mockSession,
+    });
     (postModel.deletePostById as jest.Mock).mockResolvedValue({
-      _id: mockReq.body.postId,
+      _id: new ObjectId(mockReq.params!.postId),
       creator: new ObjectId(),
-      text: "fake post test",
+      text: "fake post text",
     } as WithId<postModel.Post>);
+    (commentModel.deleteCommentsByPostId as jest.Mock).mockResolvedValue({
+      acknowledged: true,
+    });
+    (userModel.pullPostFromUser as jest.Mock).mockReturnValue({
+      acknowledged: true,
+    });
 
     await postController.deletePost(
-      mockReq as express.Request<{postId: string}>,
+      mockReq as express.Request<{ postId: string }>,
       mockRes as express.Response,
       mockNext
     );
 
+    expect(mockSession.commitTransaction).toHaveBeenCalled();
     expect(mockRes.status).toHaveBeenCalledWith(204);
     expect(mockRes.json).toHaveBeenCalledWith({
-      message: `Post ID=${mockReq.body.postId} deleted`,
+      message: `Deleted Post with ID=${mockReq.params!.postId}`,
     });
   });
 
-  it("should return 404 if deletePostById did not find Post ", async () => {
+  it("should return 404 if deletePostById fails ", async () => {
     (postModel.deletePostById as jest.Mock).mockResolvedValue(null);
 
     await postController.deletePost(
       mockReq as express.Request<{ postId: string }>,
       mockRes as express.Response,
-      mockNext as express.NextFunction
+      mockNext
     );
 
+    expect(mockSession.abortTransaction).toHaveBeenCalled();
     expect(mockNext).toHaveBeenCalledWith(
-      new ApiError(`Could not find Post ID=${mockReq.body.postId}`, 404)
+      new ApiError(`Could not find Post with ID=${mockReq.params!.postId}`, 404)
     );
   });
 
-  describe("getPostController", () => {
-    let mockReq: Partial<express.Request<{ postId: string }>>;
-    let mockRes: Partial<express.Response>;
-    let mockNext: express.NextFunction;
+  it("should return next 500 if deleteCommentsByPostId fails", async () => {
+    (postModel.deletePostById as jest.Mock).mockResolvedValue({
+      _id: new ObjectId(),
+      creator: new ObjectId(),
+      text: "fake post text",
+    } as WithId<postModel.Post>);
 
-    beforeEach(() => {
-      mockReq = { params: { postId: new ObjectId().toString() } };
-      mockRes = {
-        status: jest.fn().mockReturnThis(),
-        json: jest.fn(),
-      };
-      mockNext = jest.fn();
+    (commentModel.deleteCommentsByPostId as jest.Mock).mockResolvedValue({
+      acknowledged: false,
     });
 
-    it("should return 200 and the Post if Post ID exists", async () => {
-      const mockPost: postModel.Post = {
-        creator: new ObjectId(),
-        text: "fake post text",
-      };
+    await postController.deletePost(
+      mockReq as express.Request<{ postId: string }>,
+      mockRes as express.Response,
+      mockNext
+    );
 
-      (postModel.findPostById as jest.Mock).mockReturnValue(mockPost);
+    expect(mockSession.abortTransaction).toHaveBeenCalled();
+    expect(mockNext).toHaveBeenCalledWith(new ApiError("DB Error", 500));
+  });
 
-      await postController.getPost(
-        mockReq as express.Request<{ postId: string }, {}>,
-        mockRes as express.Response,
-        mockNext
-      );
-
-      expect(mockRes.status).toHaveBeenCalledWith(200);
-      expect(mockRes.json).toHaveBeenCalledWith(mockPost);
+  it("should return next with 500 if pullPostFromUser fails", async () => {
+    (postModel.deletePostById as jest.Mock).mockResolvedValue({
+      _id: new ObjectId(),
+      creator: new ObjectId(),
+      text: "fake post text",
+    } as WithId<postModel.Post>);
+    (commentModel.deleteCommentsByPostId as jest.Mock).mockResolvedValue({
+      acknowledged: true,
+    });
+    (userModel.pullPostFromUser as jest.Mock).mockResolvedValue({
+      acknowledge: false,
     });
 
-    it("should return 404 if Post ID does not exist", async () => {
-      (postModel.findPostById as jest.Mock).mockReturnValue(null);
+    await postController.deletePost(
+      mockReq as express.Request<{ postId: string }>,
+      mockRes as express.Response,
+      mockNext
+    );
 
-      await postController.getPost(
-        mockReq as express.Request<{ postId: string }, {}>,
-        mockRes as express.Response,
-        mockNext
-      );
+    expect(mockSession.abortTransaction).toHaveBeenCalled();
+    expect(mockNext).toHaveBeenCalledWith(new ApiError("DB Error", 500));
+  });
+});
 
-      expect(mockNext).toHaveBeenCalledWith(
-        new ApiError(`Could not find Post ID=${mockReq.params!.postId}`, 404)
-      );
-    });
+describe("getPostController", () => {
+  let mockReq: Partial<express.Request>;
+  let mockRes: Partial<express.Response>;
+  let mockNext: express.NextFunction;
+
+  beforeEach(() => {
+    mockReq = { params: { postId: new ObjectId().toString() } };
+    mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    mockNext = jest.fn()
+  });
+
+  it("should return 200 and Post if success", async () => {
+    const mockPost: postModel.Post = {creator: new ObjectId(),
+      text: "fake post text",};
+
+    (postModel.findPostById as jest.Mock).mockResolvedValue(mockPost);
+
+    await postController.getPost(
+      mockReq as express.Request<{postId: string}>,
+      mockRes as express.Response,
+      mockNext
+    );
+
+    expect(mockRes.status).toHaveBeenCalledWith(200);
+    expect(mockRes.json).toHaveBeenCalledWith(mockPost);
+  });
+
+  it("should return 404 if failure", async () => {
+    (postModel.findPostById as jest.Mock).mockResolvedValue(null);
+
+    await postController.getPost(
+      mockReq as express.Request<{postId: string}>,
+      mockRes as express.Response,
+      mockNext
+    );
+
+    expect(mockNext).toHaveBeenCalledWith(new ApiError(`Could not find Post ID=${mockReq.params!.postId}`, 404));
   });
 });
