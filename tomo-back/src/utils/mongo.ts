02e0ff80ac3ac072130883b9f4865dea7f2651b1
fealypino <njfealy@gmail.com>
2025-05-27 12:30:21 -0500

    Implement mongoDB connection
    Implement conditional connection to docker-compose mongo container or docker mongo container

---
+++ tomo-back/src/utils/mongo.ts
@@ -0,0 +1,43 @@
+import { MongoClient, ServerApiVersion, Db } from "mongodb";
+
+let mongoUri;
+if (process.env.NODE_ENV === "local") {
+  mongoUri = process.env.MONGO_LOCAL_URI!;
+
+  console.log(process.env.MONGO_LOCAL_URI || "yay")
+} else {
+  mongoUri = process.env.MONGO_DOCKER_URI!;
+}
+
+let client: MongoClient;
+let db: Db;
+
+export const connectToMongo = async () => {
+  if (client) return client;
+  try {
+    client = new MongoClient(mongoUri, {
+      serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+      },
+    });
+    await client.connect();
+    console.log("Connected to MongoDB");
+    db = client.db("tomo");
+    return client;
+  } catch (error) {
+    console.error("Failed to connect to MongoDB", error);
+    throw error;
+  }
+};
+
+export const getClient = () => {
+  if (!client) throw new Error("MongoDB client not connected");
+  return client;
+};
+
+export const getDb = () => {
+  if (!client) throw new Error("MongoDB client not connected");
+  return db;
+};
