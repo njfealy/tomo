d86758a895f766f8130f7de0228090febdc46e88
fealypino <njfealy@gmail.com>
2025-06-18 12:33:50 -0500

    Allow CORS and implement SocketIO

--- tomo-back/src/app.ts
+++ tomo-back/src/app.ts
@@ -9,40 +9,44 @@ if (process.env.NODE_ENV === "local") {
 }
 
 import express from "express";
-import "./types/global"
+import "./types/global";
 import http from "http";
-import session from "express-session";
 
 import postRoutes from "./post/post-routes";
 import userRoutes from "./user/user-routes";
-import authRoutes from "./auth/auth-routes"
+import authRoutes from "./auth/auth-routes";
 import { errorHandler } from "./middleware/error-handler";
 
 import { connectToMongo } from "./utils/mongo";
 import passport from "passport";
-import "./auth/passport-config"
+import "./auth/passport-config";
+import { sessionMiddleware } from "./middleware/session";
+
+import { Server as SocketIOServer } from "socket.io";
+import cors from "cors";
+import { setupSocket } from "./utils/socket";
 
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
 app.use(
-  session({
-    secret: "supersecret",
-    resave: false,
-    saveUninitialized: false,
+  cors({
+    origin: "http://localhost:3000",
+    credentials: true,
   })
 );
-
+app.use(sessionMiddleware);
 app.use(passport.initialize());
 app.use(passport.session());
 
 app.use("/posts", postRoutes);
 app.use("/users", userRoutes);
-app.use("/auth", authRoutes)
+app.use("/auth", authRoutes);
 app.use(errorHandler);
 
 const server = http.createServer(app);
+const io = setupSocket(server);
 
 connectToMongo()
   .then(() =>
