ba4147e952c97685379d13785c8ef1434b412f4c
fealypino <njfealy@gmail.com>
2025-09-10 15:19:35 -0500

    Added friend request model layer

--- tomo-back/src/friend-request/friend-request-model.ts
+++ tomo-back/src/friend-request/friend-request-model.ts
@@ -35,3 +35,66 @@ export const pullFriendRequest = async (
 
   return await collection.findOneAndDelete({ _id: requestId }, { session });
 };
+
+export const getUserFriendRequests = async (userId: ObjectId) => {
+  const collection = getFriendRequestCollection();
+
+  return await collection
+    .aggregate([
+      {
+        $match: {
+          receiverId: userId,
+        },
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "senderId",
+          foreignField: "_id",
+          as: "sender",
+        },
+      },
+      {
+        $unwind: "$sender",
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "receiverId",
+          foreignField: "_id",
+          as: "receiver",
+        },
+      },
+      {
+        $unwind: "$receiver",
+      },
+      {
+        $set: {
+          mutualFriends: {
+            $setIntersection: ["$sender.friends", "$receiver.friends"]
+          }
+        }
+      },
+      {
+        $lookup: {
+          from: "users",
+          localField: "mutualFriends",
+          foreignField: "_id",
+          as: "mutualFriends",
+        },
+      },
+      {
+        $project: {
+          _id: 1,
+          sentAt: 1,
+          "sender._id": 1,
+          "sender.displayName": 1,
+          "sender.pictureUri": 1,
+          "mutualFriends._id": 1,
+          "mutualFriends.displayName": 1,
+          "mutualFriends.pictureUri": 1,
+        },
+      },
+    ])
+    .toArray();
+};
