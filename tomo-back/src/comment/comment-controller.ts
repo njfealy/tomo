910b9ba07754d44d8ac7b6348e1cfd2a8bb3bee8
fealypino <njfealy@gmail.com>
2025-09-10 15:17:59 -0500

    Added liking/unliking and replying functions to comments.

--- tomo-back/src/comment/comment-controller.ts
+++ tomo-back/src/comment/comment-controller.ts
@@ -1,47 +1,60 @@
 import express from "express";
-import {
-  MakeCommentRequest,
-  DeleteCommentRequest,
-  EditCommentRequest,
-} from "./comment.types";
 
 import {
   insertComment,
   deleteCommentById,
   updateCommentById,
+  findCommentById,
+  addLikeToComment,
+  pullLikeFromComment,
+  pushReplyToComment,
+  pushLikeToReply,
+  pullLikeFromReply,
 } from "./comment-model";
 import { addCommentToPost, pullCommentFromPost } from "../post/post-model";
 
 import { ObjectId } from "mongodb";
 import { getClient } from "@App/utils/mongo";
 import { ApiError } from "@App/utils/api-error";
+import { addEngagement, removeEngagement } from "@App/utils/trending";
 
 export const makeComment = async (
-  req: express.Request<{ postId: string }, {}, MakeCommentRequest>,
+  req: express.Request<{ postId: string }, {}, { commentText: string }>,
   res: express.Response,
   next: express.NextFunction
 ) => {
+  if (!req.user) return next(new ApiError("Unauthorized", 401));
   const postId = new ObjectId(req.params.postId);
-  const creatorId = new ObjectId(req.body.creatorId);
-  const commentText = req.body.text;
+  const creatorId = new ObjectId(req.user._id);
+  const commentText = req.body.commentText;
 
   const session = getClient().startSession();
+  let commentId: ObjectId;
   try {
     session.startTransaction();
 
+    const now = new Date();
     const insertCommentResult = await insertComment(
       postId,
       creatorId,
       commentText,
+      now,
       session
     );
     if (!insertCommentResult || !insertCommentResult.acknowledged)
       throw new ApiError("DB Error", 500);
 
-    const commentId = insertCommentResult.insertedId;
+    commentId = insertCommentResult.insertedId;
 
     const addToPostResult = await addCommentToPost(postId, commentId, session);
     if (!addToPostResult) throw new ApiError("DB Error");
+
+    await addEngagement(
+      postId.toString(),
+      "comments",
+      creatorId.toString(),
+      now
+    );
 
     await session.commitTransaction();
     await session.endSession();
@@ -51,26 +64,31 @@ export const makeComment = async (
     return next(error);
   }
 
-  return res
-    .status(201)
-    .json({ message: `Comment made on Post ID =${postId}` });
+  return res.status(201).json({
+    message: `Comment made on Post ID =${postId}`,
+    commentId: commentId.toString(),
+  });
 };
 
 export const deleteComment = async (
-  req: express.Request<{ postId: string }, {}, DeleteCommentRequest>,
+  req: express.Request<{ postId: string; commentId: string }>,
   res: express.Response,
   next: express.NextFunction
 ) => {
+  if (!req.user) return next(new ApiError("Unauthorized", 401));
   const postId = new ObjectId(req.params.postId);
-  const commentId = new ObjectId(req.body.commentId);
+  const commentId = new ObjectId(req.params.commentId);
 
   const session = getClient().startSession();
   try {
     session.startTransaction();
 
-    const deleteCommentResult = await deleteCommentById(commentId);
+    const deleteCommentResult = await deleteCommentById(commentId, session);
     if (!deleteCommentResult)
       throw new ApiError(`Could not find Comment with ID=${commentId}`, 404);
+
+    if (deleteCommentResult.creator.toString() != req.user._id)
+      throw new ApiError("Unauthorized", 401);
 
     const pullCommentFromPostResult = await pullCommentFromPost(
       postId,
@@ -79,6 +97,13 @@ export const deleteComment = async (
     );
     if (!pullCommentFromPostResult)
       throw new ApiError(`Could not find Post with ID=${postId}`, 404);
+
+    await removeEngagement(
+      "comments",
+      postId.toString(),
+      deleteCommentResult.creator.toString(),
+      deleteCommentResult.date.getTime()
+    );
 
     await session.commitTransaction();
     await session.endSession();
@@ -94,19 +119,125 @@ export const deleteComment = async (
 };
 
 export const editComment = async (
-  req: express.Request<{}, {}, EditCommentRequest>,
+  req: express.Request<{ commentId: string }, {}, { newText: string }>,
   res: express.Response,
   next: express.NextFunction
 ) => {
-  const commentId = new ObjectId(req.body.commentId);
+  if (!req.user) return next(new ApiError("Unauthorized", 401));
+
+  const commentId = new ObjectId(req.params.commentId);
   const newText = req.body.newText;
+
+  const comment = await findCommentById(commentId);
+  if (!comment)
+    return next(
+      new ApiError(`Could not find Comment with ID=${commentId}`, 404)
+    );
+  if (comment.creator.toString() != req.user._id)
+    return next(new ApiError("Unauthorized", 401));
 
   const editResult = await updateCommentById(commentId, newText);
   if (!editResult)
     return next(
       new ApiError(`Could not find Comment with ID=${commentId}`, 404)
     );
+
   return res
     .status(204)
     .json({ message: `Edited Comment with ID=${commentId}` });
+};
+
+export const likeComment = async (
+  req: express.Request<{ commentId: string }>,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  if (!req.user) return next(new ApiError("Unauthorized", 401));
+
+  const commentId = new ObjectId(req.params.commentId);
+  const userId = new ObjectId(req.user._id);
+
+  const updateResult = await addLikeToComment(commentId, userId);
+  if (!updateResult) return next(new ApiError("Comment not found", 404));
+
+  return res.status(200).json({
+    message: "Successfully liked comment",
+  });
+};
+
+export const unlikeComment = async (
+  req: express.Request<{ commentId: string }>,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  if (!req.user) return next(new ApiError("Unauthorized", 401));
+
+  const commentId = new ObjectId(req.params.commentId);
+  const userId = new ObjectId(req.user._id);
+
+  const updateResult = await pullLikeFromComment(commentId, userId);
+  if (!updateResult) return next(new ApiError("Comment not found", 404));
+
+  return res.status(200).json({
+    message: "Successfully unliked comment",
+  });
+};
+
+export const replyToComment = async (
+  req: express.Request<{ commentId: string }, {}, { replyText: string }>,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  if (!req.user) return next(new ApiError("Unauthorized", 401));
+
+  const userId = new ObjectId(req.user._id);
+  //const postId = new ObjectId(req.params.postId);
+  const parentId = new ObjectId(req.params.commentId);
+
+  const replyText = req.body.replyText;
+
+  const pushResult = await pushReplyToComment(parentId, userId, replyText);
+  if (!pushResult) return next(new ApiError("Failed to reply to comment", 500));
+
+  return res.status(200).json({ insertedId: pushResult.replyId });
+};
+
+export const likeReply = async (
+  req: express.Request<{ commentId: string; replyId: string }>,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  if (!req.user) return next(new ApiError("Unauthorized", 401));
+
+  const commentId = new ObjectId(req.params.commentId);
+  const replyId = new ObjectId(req.params.replyId);
+  const userId = new ObjectId(req.user._id);
+
+  const likeResult = await pushLikeToReply(userId, commentId, replyId);
+  if (!likeResult.acknowledged)
+    return next(new ApiError("Failed to like reply", 500));
+
+  return res.status(200).json({
+    message: "Successfully liked reply",
+  });
+};
+
+export const unlikeReply = async (
+  req: express.Request<{ commentId: string; replyId: string }>,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  if (!req.user) return next(new ApiError("Unauthorized", 401));
+
+  const commentId = new ObjectId(req.params.commentId);
+  const replyId = new ObjectId(req.params.replyId);
+  const userId = new ObjectId(req.user._id);
+
+  const likeResult = await pullLikeFromReply(userId, commentId, replyId);
+  if (!likeResult.acknowledged)
+    return next(new ApiError("Failed to unlike reply", 500));
+
+  return res.status(200).json({
+    message: "Successfully unliked reply",
+  });
 };
--- tomo-back/src/comment/comment-model.ts
+++ tomo-back/src/comment/comment-model.ts
@@ -1,12 +1,22 @@
 import { ObjectId, ClientSession } from "mongodb";
 import { getDb } from "../utils/mongo";
-import { get } from "http";
+
+export interface Reply {
+  _id: ObjectId;
+  creator: ObjectId;
+  //mentions: ObjectId[];
+  replyText: string;
+  date: Date;
+  likes: ObjectId[];
+}
 
 export interface Comment {
   post: ObjectId;
   creator: ObjectId;
   text: string;
-  likes?: ObjectId[];
+  likes: ObjectId[];
+  date: Date;
+  replies: Reply[];
 }
 
 const getCommentCollection = () => {
@@ -17,6 +27,7 @@ export const insertComment = async (
   postId: ObjectId,
   creatorId: ObjectId,
   text: string,
+  date: Date,
   session?: ClientSession
 ) => {
   const collection = getCommentCollection();
@@ -24,6 +35,9 @@ export const insertComment = async (
     post: postId,
     creator: creatorId,
     text,
+    date,
+    likes: [],
+    replies: [],
   };
 
   return await collection.insertOne(document, { session });
@@ -73,3 +87,128 @@ export const deleteCommentsByUserId = async (
 
   return await collection.deleteMany({ creator: userId }, { session });
 };
+
+export const addLikeToComment = async (
+  commentId: ObjectId,
+  userId: ObjectId,
+  session?: ClientSession
+) => {
+  const collection = getCommentCollection();
+
+  return await collection.findOneAndUpdate(
+    { _id: commentId },
+    {
+      $addToSet: {
+        likes: userId,
+      },
+    },
+    {
+      session,
+    }
+  );
+};
+
+export const pullLikeFromComment = async (
+  commentId: ObjectId,
+  userId: ObjectId,
+  session?: ClientSession
+) => {
+  const collection = getCommentCollection();
+
+  return await collection.findOneAndUpdate(
+    { _id: commentId },
+    {
+      $pull: {
+        likes: userId,
+      },
+    },
+    {
+      session,
+    }
+  );
+};
+
+export const pushReplyToComment = async (
+  commentId: ObjectId,
+  userId: ObjectId,
+  replyText: string,
+  session?: ClientSession
+) => {
+  const collection = getCommentCollection();
+
+  const _id = new ObjectId();
+  const newReply: Reply = {
+    _id,
+    creator: userId,
+    replyText,
+    date: new Date(),
+    likes: [],
+  };
+
+  const updateResult = await collection.findOneAndUpdate(
+    { _id: commentId },
+    {
+      $push: {
+        replies: newReply,
+      },
+    },
+    {
+      session,
+    }
+  );
+
+  if (updateResult)
+    return { replyId: _id.toString(), updatedComment: updateResult };
+  return null;
+};
+
+export const pushLikeToReply = async (
+  userId: ObjectId,
+  commentId: ObjectId,
+  replyId: ObjectId
+) => {
+  const collection = getCommentCollection();
+
+  const result = await collection.updateOne(
+    {
+      _id: commentId,
+      "replies._id": replyId,
+      "replies.likes": { $ne: userId },
+    },
+    {
+      $addToSet: { "replies.$[reply].likes": userId },
+    },
+    {
+      arrayFilters: [{ "reply._id": replyId }],
+    }
+  );
+
+  return result;
+};
+
+export const pullLikeFromReply = async (
+  userId: ObjectId,
+  commentId: ObjectId,
+  replyId: ObjectId,
+) => {
+  const collection = getCommentCollection();
+  
+  const result = await collection.updateOne(
+    {
+      _id: commentId,
+      "replies._id": replyId,
+      "replies.likes": { $ne: userId }
+    },
+    {
+      $pull: { "replies.$[reply].likes": userId }
+    },
+    {
+      arrayFilters: [
+        { "reply._id": replyId }
+      ]
+    }
+  );
+
+  return result;
+    
+}
