558fdb49308bafe27c74a24d4c52ad8986e1ad4b
fealypino <njfealy@gmail.com>
2025-09-10 15:16:45 -0500

    Added Google OAuth2.0 strategy and configuration for sign-in.

--- tomo-back/src/auth/auth-routes.ts
+++ tomo-back/src/auth/auth-routes.ts
@@ -1,21 +1,25 @@
+import { getUserNotifications } from "@App/notification/notification-model";
+import { getUserPicture } from "@App/user/user-model";
 import express from "express";
+import { ObjectId } from "mongodb";
 const router = express.Router();
 
 import passport from "passport";
 
-router.get(
-  "/logout",
-  (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    req.logout((err) => {
-      if (err) return next(err);
-      res.status(200);
+router.get("/logout", (req, res, next) => {
+  req.logout((err) => {
+    if (err) return next(err);
+    req.session.destroy(() => {
+      res.clearCookie("connect.sid");
+      res.status(200).send("Logged out");
     });
-  }
-);
+  });
+});
 
 router.get(
   "/status",
   (req: express.Request, res: express.Response, next: express.NextFunction) => {
+    console.log("Auth status");
     if (req.isAuthenticated()) {
       return res.json({ user: req.user });
     }
@@ -26,9 +30,24 @@ router.get(
   "/google/redirect",
   passport.authenticate("google"),
   (req: express.Request, res: express.Response, next: express.NextFunction) => {
-    res.redirect("http://localhost:5001/auth/status");
+    res.redirect("http://localhost:3000/home");
   }
 );
 router.get("/google", passport.authenticate("google", { scope: ["profile"] }));
+
+router.get(
+  "/me",
+  async (
+    req: express.Request,
+    res: express.Response,
+    next: express.NextFunction
+  ) => {
+    console.log("me: ", req.user);
+    if (!req.user) return res.status(401).send("Unauthorized");
+    const userId = new ObjectId(req.user._id);
+    const user = await getUserPicture(userId);
+    res.status(200).json({ _id: req.user._id, displayName: req.user.displayName, pictureUri: user!.pictureUri });
+  }
+);
 
 export default router;
--- tomo-back/src/auth/passport-config.ts
+++ tomo-back/src/auth/passport-config.ts
@@ -2,18 +2,21 @@ import passport from "passport";
 import { Strategy as GoogleStrategy } from "passport-google-oauth20";
 import * as userModel from "@App/user/user-model";
 import { ApiError } from "@App/utils/api-error";
-
+import { ObjectId } from "mongodb";
 
 passport.serializeUser((user, done) => {
-  return done(null, user.googleId);
+  console.log("serializing user: ",user)
+  return done(null, user._id.toString());
 });
 
 passport.deserializeUser(async (id: string, done) => {
-  const user = await userModel.findUserByGoogleId(id);
+  console.log("deserializing id: ", id)
+  const _id = new ObjectId(id)
+  const user = await userModel.findPublicUserByUserId(_id);
   return done(null, {
-    googleId: user!.googleId,
+    _id: user!._id.toString(),
     displayName: user!.displayName,
-  } );
+  });
 });
 
 passport.use(
@@ -21,22 +24,31 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      //callbackURL: "https://oauth.pstmn.io/v1/browser-callback",
       callbackURL: "http://localhost:5001/auth/google/redirect",
       scope: ["profile"],
       state: true,
     },
     async (accessToken, refreshToken, profile, done) => {
       if (!profile.id) throw new ApiError("Auth Error", 500);
+      console.log(profile)
       const user = await userModel.findUserByGoogleId(profile.id);
+      console.log("user:", user)
+      let _id: ObjectId;
       if (!user) {
         const result = await userModel.insertUser(
           profile.displayName,
-          profile.id
+          profile.id,
+          profile._json.picture
         );
-      } else { console.log("hello ", user.displayName)}
+        _id = result.insertedId;
+      } else {
+        console.log("hello ", user.displayName);
+        _id = user._id;
+      }
       return done(null, {
+        _id: _id.toString(),
         displayName: profile.displayName,
-        googleId: profile.id,
       } as Express.User);
     }
   )
